d78d742ea55052db0568042420aca50c572e1c43
Timothy Merritt <timmybytes@gmail.com>
2020-08-31 12:52:17 -0700

    Updates

---
+++ precache-manifest.34d131bdef620bedceecf3d36fc27d7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b1165b276653977ca7306531b2b0af",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "6048dda3b34cc79b7879",
+    "url": "/test-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d450a65a2ea0ab6d358",
+    "url": "/test-app/static/js/2.e5f0e2ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-app/static/js/2.e5f0e2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6048dda3b34cc79b7879",
+    "url": "/test-app/static/js/main.c535a2e5.chunk.js"
+  },
+  {
+    "revision": "010976abd684515c15d4",
+    "url": "/test-app/static/js/runtime-main.107bf183.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
